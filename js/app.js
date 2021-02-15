b6dd55325569e8741cce336bdfd53811b1ffb25a
Christopher <chris_bernhard@yahoo.com>
2021-02-15 13:40:18 -0600

    styled the page

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ const mapid = document.querySelector('#mapid');
 //FUNCTIONS
 function makeMap(coords, popUpMessage) {
   mapid.classList.remove('hidden');
+  geoButton.classList.add('hidden');
   mymap.setView(coords, 15);
 
   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
